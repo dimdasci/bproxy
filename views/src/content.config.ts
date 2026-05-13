b8c8ce9e7c2cbf347c69169f6c53d5de6db94015
Dmitrii Kharitonov <dimds@fastmail.com>
2026-05-13 16:47:37 +0200

    feat(views): add docs + views content collections with shared view schema

---
+++ views/src/content.config.ts
@@ -0,0 +1,22 @@
+import { defineCollection } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
+import { glob } from 'astro/loaders';
+import { viewSchema } from './lib/view-schema';
+
+const docs = defineCollection({
+  loader: glob({
+    pattern: ['**/*.{md,mdx}', '!views/**'],
+    base: '../docs',
+  }),
+  schema: docsSchema(),
+});
+
+const views = defineCollection({
+  loader: glob({
+    pattern: '**/*.md',
+    base: '../docs/views',
+  }),
+  schema: docsSchema({ extend: viewSchema }),
+});
+
+export const collections = { docs, views };
